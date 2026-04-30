f833969d177711ec7fca42153b3af4cb278a10e8
Debugger Doctor <doctor.cybersec122@gmail.com>
2026-04-30 13:25:11 -0300

    initial commit

---
+++ modules/hosts/my-machine/hardware-configuration.nix
@@ -0,0 +1,33 @@
+{ self, inputs, ... }: {
+
+  flake.nixosModules.myMachineHardware = { config, lib, pkgs, modulesPath, ... }: {
+    imports =
+      [ (modulesPath + "/installer/scan/not-detected.nix")
+      ];
+
+    boot.initrd.availableKernelModules = [ "xhci_pci" "ahci" "usbhid" "usb_storage" "sd_mod" ];
+    boot.initrd.kernelModules = [ ];
+    boot.kernelModules = [ "kvm-intel" ];
+    boot.extraModulePackages = [ ];
+
+    fileSystems."/" =
+      { device = "/dev/disk/by-uuid/2cc84afb-da56-44a5-936f-40d76723d77d";
+        fsType = "ext4";
+      };
+
+    fileSystems."/boot" =
+      { device = "/dev/disk/by-uuid/CC5C-4657";
+        fsType = "vfat";
+        options = [ "fmask=0077" "dmask=0077" ];
+      };
+
+    swapDevices =
+      [ { device = "/dev/disk/by-uuid/1197a9a7-ab94-4c92-b379-e903d3f58eab"; }
+      ];
+
+    nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
+    hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  };
+
+
+}
